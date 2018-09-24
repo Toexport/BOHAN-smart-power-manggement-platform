de9ff0060726f50466e25b300c538c2aedbb5bf4
zhaoning <zhaoningdeemail@qq.com>
2018-09-24 15:56:58 +0800

    啊啊

---
+++ Bohan IOS源码/Bohan/Bohan/Class/ Prepaid/Controller/MyAccount/AccountCenter/WithdrawalsCenter/WithdrawalsView/WithdrawalsView.h
@@ -0,0 +1,25 @@
+//
+//  WithdrawalsView.h
+//  Bohan
+//
+//  Created by summer on 2018/9/20.
+//  Copyright © 2018年 Bohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//#import "NSDate+Extension.h"
+
+@interface WithdrawalsView : UIView
+@property (weak, nonatomic) IBOutlet UIView *buttomView;
+@property (weak, nonatomic) IBOutlet UIButton *doneBtn;
+@property (weak, nonatomic) IBOutlet UITableView *Ttableview;
+
+/**
+ 空白VIEW
+ */
+-(instancetype)initWithDateStyle:(NSInteger)datePickerStyle BlankBlock:(void(^)(NSDate *))completeBlock;
+
+-(void)show;
+
+@end
+
